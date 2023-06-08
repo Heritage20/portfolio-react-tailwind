37a4019b12e5645d423f6e75b888767587496fa8
Abraham Fatoki <fatokiabraham@gmail.com>
2023-06-08 19:50:30 +0100

    fix: styles in footer

--- src/scenes/Footer.tsx
+++ src/scenes/Footer.tsx
@@ -4,14 +4,14 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="h-64 bg-grey pt-10">
+    <footer className="h-64 bg-red pt-10">
       <div className="w-5/6 mx-auto">
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center">
-          <p className="font-playfair font-semibold text-2xl text-deep-blue">
+          <p className="font-playfair font-semibold text-2xl text-grey">
             JANE ESPER
           </p>
-          <p className="font-playfair text-md text-deep-blue">
+          <p className="font-playfair text-md text-grey">
             © {year} ESPER. Made with ❤️ from Nigeria.
           </p>
         </div>
